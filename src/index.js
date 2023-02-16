8e9abf2f3abe315b6f186b6fe0c4738c283823cd
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-02-16 18:28:36 -0300

    rendering router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Navbar from './Components/Navbar';
+import Router from 'routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
+    <Router/>
   </React.StrictMode>
 );
