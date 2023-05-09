75b35f3ba7b9710c5cdfb49ec29b253e7e9a3175
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-05-08 22:47:58 -0300

    picking up the items

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,19 +1,29 @@
 import Header from "components/Header";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import styles from "./Categoria.module.scss";
 
-export default function Categoria(){
-  const {nomeCategoria} = useParams();
-  const categoria = useSelector(
-  state => state.categorias.find(categoria => categoria.id === nomeCategoria)
-  )
+export default function Categoria() {
+  const { nomeCategoria } = useParams();
+  const { categoria, itens } = useSelector((state) => ({
+    categoria: state.categorias.find(
+      (categoria) => categoria.id === nomeCategoria
+    ),
+    itens: state.itens.filter((item) => item.categoria === nomeCategoria),
+  }));
+  console.log(itens);
   return (
     <div>
       <Header
-      titulo={categoria.nome}
-      descricao={categoria.descricao}
-      imagem={categoria.header}
+        titulo={categoria.nome}
+        descricao={categoria.descricao}
+        imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map((item) => (
+          <div key={item.id}>{item.titulo}</div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
