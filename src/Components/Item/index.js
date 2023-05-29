3e737d272d0ba623d6c7ea1dd3d9a4994f6902dd
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-05-29 17:16:43 -0300

    Applying the Reducer

--- src/Components/Item/index.js
+++ src/Components/Item/index.js
@@ -1,8 +1,9 @@
 import styles from "./Item.module.scss";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import {FaCartPlus} from 'react-icons/fa';
+import { FaCartPlus } from "react-icons/fa";
 import { mudarFavorito } from "store/reducers/itens";
-import {  useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { mudarCarrinho } from "store/reducers/carrinho";
 
 const iconeProps = {
   size: 34,
@@ -10,20 +11,20 @@ const iconeProps = {
 };
 
 export default function Item(props) {
-  const { titulo,
-     foto,
-      preco,
-       descricao,
-        favorito,
-        id,
-
-       } = props;
+  const { titulo, foto, preco, descricao, favorito, id } = props;
 
   const dispatch = useDispatch();
+  const estaNoCarrinho = useSelector((state) =>
+    state.carrinho.some((itemNoCarrinho) => itemNoCarrinho.id === id)
+  );
 
-  function resolverFavorito(){
+  function resolverFavorito() {
     dispatch(mudarFavorito(id));
-  } 
+  }
+
+  function resolverCarrinho() {
+    dispatch(mudarCarrinho(id));
+  }
 
   return (
     <div className={styles.item}>
@@ -46,16 +47,17 @@ export default function Item(props) {
                 onClick={resolverFavorito}
               />
             ) : (
-              <AiOutlineHeart 
-              {...iconeProps}
-              className={styles["item-acao"]}
-              onClick={resolverFavorito}
+              <AiOutlineHeart
+                {...iconeProps}
+                className={styles["item-acao"]}
+                onClick={resolverFavorito}
               />
             )}
             <FaCartPlus
               {...iconeProps}
-              color={false ? "#1875E8" : iconeProps.color}
-              className={styles['item-acao']}
+              color={estaNoCarrinho ? "#1875E8" : iconeProps.color}
+              className={styles["item-acao"]}
+              onClick={resolverCarrinho}
             />
           </div>
         </div>
